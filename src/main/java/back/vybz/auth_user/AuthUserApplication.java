bac5c507fd4d0ec0e5925c13f1a4f1550de561aa
jijihorang <jiho7213@gmail.com>
2025-06-03 01:41:07 +0900

    feat: auth-user 서비스 프로젝트 초기 설정

---
+++ src/main/java/back/vybz/auth_user/AuthUserApplication.java
@@ -0,0 +1,13 @@
+package back.vybz.auth_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/back/vybz/auth_user/AuthUserApplicationTests.java
@@ -0,0 +1,13 @@
+package back.vybz.auth_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
