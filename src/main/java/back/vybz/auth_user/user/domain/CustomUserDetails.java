901a4608aed0810aea0a08a6bb1679eaf1db3b56
jijihorang <jiho7213@gmail.com>
2025-06-04 11:30:15 +0900

    feat: auth-user social login 기능 구현 및 deploy.yml 수정

---
+++ src/main/java/back/vybz/auth_user/user/domain/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package back.vybz.auth_user.user.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserUuid();
+    }
+
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getStatus().name().equals("ACTIVE");
+    }
+}
---
+++ src/main/java/back/vybz/auth_user/user/domain/User.java
@@ -0,0 +1,61 @@
+package back.vybz.auth_user.user.domain;
+
+import back.vybz.auth_user.user.common.entity.SoftDeletableEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+public class User extends SoftDeletableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 사용자 uuid
+     */
+    @Column(name = "user_uuid", nullable = false, unique = true)
+    private String userUuid;
+
+    /**
+     * 소셜 타입
+     */
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    /**
+     * 소셜 id
+     */
+    @Column(name = "provider_id", unique = true, nullable = false, length = 20)
+    private String providerId;
+
+    /**
+     * 이메일
+     */
+    @Column(name = "email", nullable = false, unique = true, length = 30)
+    private String email;
+
+    /**
+     * 사용자 상태
+     */
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Builder
+    public User(Long id, String userUuid, SocialType socialType, String providerId,
+                String email, Status status) {
+        this.id = id;
+        this.userUuid = userUuid;
+        this.socialType = socialType;
+        this.providerId = providerId;
+        this.email = email;
+        this.status = status;
+    }
+}
