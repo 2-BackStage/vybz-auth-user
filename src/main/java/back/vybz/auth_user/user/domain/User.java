af994f3de9813481efe89914a45aed11eeb0921c
jijihorang <jiho7213@gmail.com>
2025-06-05 12:07:01 +0900

    refactor: user provicer_id length 늘림

--- src/main/java/back/vybz/auth_user/user/domain/User.java
+++ src/main/java/back/vybz/auth_user/user/domain/User.java
@@ -32,7 +32,7 @@ public class User extends SoftDeletableEntity {
     /**
      * 소셜 id
      */
-    @Column(name = "provider_id", unique = true, nullable = false, length = 20)
+    @Column(name = "provider_id", unique = true, nullable = false, length = 30)
     private String providerId;
 
     /**
